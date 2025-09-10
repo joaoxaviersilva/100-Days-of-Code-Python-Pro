bf61686652c1197f478488338557bd6021c0bf87
João Xavier <166918086+joaoxaviersilva@users.noreply.github.com>
2025-09-10 08:08:00 -0300

    Feat: Add Band Name Generator project (Day 1)

---
+++ day-01-band-name-generator/main.py
@@ -0,0 +1,11 @@
+# Print a welcome message to the user
+print("Welcome to the Band Name Generator")
+
+# Ask the user for the city they grew up in
+name_city = input("Which city did you grow up in?\n")
+
+# Ask the user for the name of a pet
+name_pet = input("What´s the name of a pet?\n")
+
+# Combine the city and pet name to create a band name suggestion
+print(f"Your band name could be: {name_city} {name_pet}")

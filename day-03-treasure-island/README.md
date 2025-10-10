33b205f9d81170cb43fc78dfbfbaa33a3be63c7f
João Xavier <166918086+joaoxaviersilva@users.noreply.github.com>
2025-10-10 10:51:14 -0300

    Feat: Add Treasure Island project (Day 3)

---
+++ day-03-treasure-island/README.md
@@ -0,0 +1,32 @@
+#   Day 3: Treasure Island
+
+### Description
+
+A simple Python adventure game where the player makes choices to find the treasure. It uses conditional statements to control the story flow.
+
+### Technologies Used
+
+- Python 3
+- `input()`, `print()`
+- Conditional statements (`if`, `elif`, `else`)
+
+---
+
+### How to Run
+
+1. Clone this repository  
+2. Navigate to the project folder  
+3. Run the script:
+
+
+---
+
+### Demo
+
+![Treasure Island adventure](screenshot.gif)
+
+### Key Learnings
+
+- Using nested conditionals to control program logic  
+- Handling user input and converting it to lowercase for validation  
+- Structuring interactive text-based games in Python
---
+++ day-03-treasure-island/main.py
@@ -0,0 +1,61 @@
+# Print a welcome message with the treasure map art
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+
+# Welcome the player to the game and state the mission
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+# Ask the player to choose a direction at a crossroad
+choice1 = input('You\'re at a crossroad, where do you want to go? '
+                'Type "left" or "right".\n').lower()
+
+# If player chooses left, proceed to the lake scenario
+if choice1 == "left":
+    # Ask the player whether to wait for a boat or swim across the lake
+    choice2 = input('You\'ve come to a lake. '
+                    'There is an island in the middle of the lake. '
+                    'Type "wait" to wait for a boat. '
+                    'Type "swim" to swim across.\n').lower()
+    if choice2 == "wait":
+        # Upon waiting safely, ask the player to choose one of three doors
+        choice3 = input("You arrive at the island unharmed. "
+                        "There is house with 3 doors. One red, "
+                        "one yellow and one blue. "
+                        "Which colour do you choose?\n").lower()
+        # Check door choice outcomes
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over")
+        elif choice3 == "yellow":
+            print("You found the treasure. You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        # If player tries to swim, they get attacked by a trout
+        print("You got attacked by an angry trout. Game Over.")
+
+# If player chooses right at the crossroad, they fall in a hole
+else:
+    print("You fell in to a hole. Game Over.")
